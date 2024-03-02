bc66ef38d891fde50f7c47a6ef922a039e62e4ca
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-02 11:47:26 +0100

    remove node url import

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/Eisvana-Album-Entry/',
-	plugins: [
-		vue(),
-	],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		}
-	}
-})
+  base: '/Eisvana-Album-Entry/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
