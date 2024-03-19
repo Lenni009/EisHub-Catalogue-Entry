142095280e5f84d05df22cdf53a002638ef55bab
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-19 14:36:17 +0100

    store image type in variable

--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -67,15 +67,16 @@ async function compressFile(inputFile: File, quality: number = 1): Promise<File>
   const sanitisedFileName = inputFile.name.replaceAll(/['"[\]{}]/g, '_');
   const file = new File([inputFile], sanitisedFileName, { type: inputFile.type });
   if (file.size < maxSize) return file; // if below 10 MB, don't do anything
+  const type = imageTypes.JPEG;
   const res = await compressImage(file, {
     quality,
-    type: imageTypes.JPEG,
+    type,
   });
   const lowerQuality = quality - 0.01; // NoSonar reduce quality by 1%;
   if (res.size > maxSize) return await compressFile(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
   const fileName = file.name.split('.').slice(0, -1).join('.');
-  const newFileName = fileName + '-min.jpg';
-  return new File([res], newFileName, { type: imageTypes.JPEG });
+  const newFileName = `${fileName}-min.${type}`;
+  return new File([res], newFileName, { type });
 }
 
 const generateAlbumEntry = (page: string): string => albumStrings[page];
